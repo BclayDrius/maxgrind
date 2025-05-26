b2c60d68155abd58c111113a07a29a8e3a834b6c
Barclay <137096739+BclayDrius@users.noreply.github.com>
2025-05-26 15:29:50 -0500

    FRONTEND ALPHA

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,18 @@ window.addEventListener('scroll', () => {
         cardsGrid.classList.remove('expanded');
     }
 });
+function includeLayout() {
+    fetch("../components/navbar.html")
+        .then((res) => res.text())
+        .then((data) => {
+            document.getElementById("navbar").outerHTML = data;
+        });
+
+    fetch("../components/footer.html")
+        .then((res) => res.text())
+        .then((data) => {
+            document.getElementById("footer").outerHTML = data;
+        });
+}
+
+window.addEventListener("DOMContentLoaded", includeLayout);
